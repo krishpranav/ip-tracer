b18345df984c507d4503e35b4b5d081b4faad5f8
krishpranav <krisna.pranav@gmail.com>
2021-06-30 08:33:25 +0530

    [+] imports [+]

--- iptracer.py
+++ iptracer.py
@@ -1,25 +1,8 @@
-import os 
-import urllib2
-import json
+#!/usr/bin/env python3
 
-def author():
-    print("Tool Is Created By Krishna Pranav")
-    print("Github Link https://www.github.com/krishpranav")
-    print("Do not forget to follow me :)")
-    
-while True:
-    ip = input("Enter Your Target Ip Address >> ")
-    url = "http://ip-api.com/json"
-    response = urllib2.urlopen(url + str(ip))
-    data = response.read()
-    values = json.loads(data)
-    print("IP: " + values['query'])
-    print("Contiinent >> " + values['continent'])
-    print("Country >> " + values['country'])
-    print("Region >> " + values['regionName'])
-    print("City >> " + values['city'])
-    print("Zip >> " + values['zip'])
-    print("Latitude >> " + values['lat'])
-    print("Longitude >> " + values['lon'])
-    print("ISP >> " + values['isp'])
-author()
+# imports
+import argparse
+import requests, json
+import sys
+from sys import argv
+import os
