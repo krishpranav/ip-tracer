88108a2ea2f501b2c68791cbfdddd1bfa8b7a4ec
krishpranav <krisna.pranav@gmail.com>
2021-06-30 08:37:58 +0530

    [+] try block [+]

--- iptracer.py
+++ iptracer.py
@@ -33,3 +33,26 @@ banner()
 ip = args.target
 api = "https://ip.api.com/json"
 
+try:
+    data = requests.get(api+ip).json()
+    sys.stdout.flush()
+    a = lgreen+bold+"[$]"
+    b = cyan+bold+"[$]"
+    print (a, "[Victim]:", data['query'])
+    print(red+"-----------------"+red)
+    print (b, "[ISP]:", data['isp'])
+    print(red+"-----------------"+red)
+    print (a, "[Organisation]:", data['org'])
+    print(red+"-----------------"+red)
+    print (b, "[City]:", data['city'])
+    print(red+"-----------------"+red)
+    print (a, "[Region]:", data['region'])
+    print(red+"-----------------"+red)
+    print (b, "[Longitude]:", data['lon'])
+    print(red+"-----------------"+red)
+    print (a, "[Latitude]:", data['lat'])
+    print(red+"-----------------"+red)
+    print (b, "[Time zone]:", data['timezone'])
+    print(red+"-----------------"+red)
+    print (a, "[Zip code]:", data['zip'])
+    print (" "+yellow)
