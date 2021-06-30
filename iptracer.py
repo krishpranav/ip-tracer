3842c49f0619fc0b23c4ebcf970403934df48309
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2021-06-30 08:41:21 +0530

    Update iptracer.py

--- iptracer.py
+++ iptracer.py
@@ -31,35 +31,36 @@ def banner():
 banner()
 
 ip = args.target
-api = "https://ip.api.com/json"
+
+api = "http://ip-api.com/json/"
 
 try:
-    data = requests.get(api+ip).json()
-    sys.stdout.flush()
-    a = lgreen+bold+"[$]"
-    b = cyan+bold+"[$]"
-    print (a, "[Victim]:", data['query'])
-    print(red+"-----------------"+red)
-    print (b, "[ISP]:", data['isp'])
-    print(red+"-----------------"+red)
-    print (a, "[Organisation]:", data['org'])
-    print(red+"-----------------"+red)
-    print (b, "[City]:", data['city'])
-    print(red+"-----------------"+red)
-    print (a, "[Region]:", data['region'])
-    print(red+"-----------------"+red)
-    print (b, "[Longitude]:", data['lon'])
-    print(red+"-----------------"+red)
-    print (a, "[Latitude]:", data['lat'])
-    print(red+"-----------------"+red)
-    print (b, "[Time zone]:", data['timezone'])
-    print(red+"-----------------"+red)
-    print (a, "[Zip code]:", data['zip'])
-    print (" "+yellow)
+        data = requests.get(api+ip).json()
+        sys.stdout.flush()
+        a = lgreen+bold+"[$]"
+        b = cyan+bold+"[$]"
+        print (a, "[Victim]:", data['query'])
+        print(red+"<--------------->"+red)
+        print (b, "[ISP]:", data['isp'])
+        print(red+"<--------------->"+red)
+        print (a, "[Organisation]:", data['org'])
+        print(red+"<--------------->"+red)
+        print (b, "[City]:", data['city'])
+        print(red+"<--------------->"+red)
+        print (a, "[Region]:", data['region'])
+        print(red+"<--------------->"+red)
+        print (b, "[Longitude]:", data['lon'])
+        print(red+"<--------------->"+red)
+        print (a, "[Latitude]:", data['lat'])
+        print(red+"<--------------->"+red)
+        print (b, "[Time zone]:", data['timezone'])
+        print(red+"<--------------->"+red)
+        print (a, "[Zip code]:", data['zip'])
+        print (" "+yellow)
 
 except KeyboardInterrupt:
-    print('Bye :)'+lgreen)
-    sys.exit(0)
+        print ('Terminating, Bye'+lgreen)
+        sys.exit(0)
 except requests.exceptions.ConnectionError as e:
-    print (red+"[+]"+" check your internet connection!"+clear)
-    sys.exit(1)
+        print (red+"[~]"+" check your internet connection!"+clear)
+sys.exit(1)
