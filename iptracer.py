06ad9ba44b2eedfd8b35e8bea4e4ada087719f0f
krishpranav <krisna.pranav@gmail.com>
2021-06-30 08:34:13 +0530

    [+] parser [+]

--- iptracer.py
+++ iptracer.py
@@ -6,3 +6,10 @@ import requests, json
 import sys
 from sys import argv
 import os
+
+# parser values
+parser = argparse.ArgumentParse()
+
+parser.add_argument("-v", help="target/host IP address", type=str, dest='target', required=True)
+
+args = parser.parse_args()
