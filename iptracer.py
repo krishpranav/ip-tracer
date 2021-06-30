e6623b32d8f2f27436db8f465c6d1031d15a2411
krishpranav <krisna.pranav@gmail.com>
2021-06-30 08:34:33 +0530

    [+] colors [+]

--- iptracer.py
+++ iptracer.py
@@ -7,9 +7,18 @@ import sys
 from sys import argv
 import os
 
-# parser values
+# parser
 parser = argparse.ArgumentParse()
 
 parser.add_argument("-v", help="target/host IP address", type=str, dest='target', required=True)
 
 args = parser.parse_args()
+
+
+# colors
+red = '\033[31m'
+yellow = '\033[93m'
+lgreen = '\033[92m'
+clear = '\033[0m'
+bold = '\033[01m'
+cyan = '\033[96m'
